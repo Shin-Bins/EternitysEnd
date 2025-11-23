05433080c1390323a9743dfb5e5a94713a3d0656
Shin-Bins <sean_deans@hotmail.co.uk>
2025-11-23 19:15:09 +0000

    Skull PickUp
    We can now pick up the skull

--- Assets/Scripts/player_scripts/scripts_phiast/PickUpSkull.cs
+++ Assets/Scripts/player_scripts/scripts_phiast/PickUpSkull.cs
@@ -1,19 +1,90 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PickUpSkull : MonoBehaviour
 {
-bool isHolding = false;
-[SerializeField]float throwForce = 300f;
-[SerializeField]float maxDistance = 10f;
-float distance;
+	public bool inRange = false;
+	public bool isHolding = false;
+	[SerializeField]float throwForce = 300f;
+	[SerializeField]float maxDistance = 10f;
+	private float distance;
 
-HoldSkull holdSkull;
-Rigidbody rb;
-Vector3 objectPosition;
+	private Rigidbody rb;
+	private Collider cuan;
+	private Rigidbody skullRb;//this is a reference for when we pick up the skull
+	public Transform objectPosition;
+
+	private PlayerControls playerControls;
 
 void Start()
 {
 	rb = GetComponent<Rigidbody>();
-	holdSkull = HoldSkull.Instance;
+	playerControls = new PlayerControls();
+	SetUpControls();
+}
+
+
+
+void OnTriggerEnter(Collider other)
+{
+	if(other.CompareTag("skull"))
+	{
+		inRange = true;
+		cuan = other;
+	}
+}
+
+void OnTriggerExit(Collider other)
+{
+	if(other.CompareTag("skull"))
+	{
+		inRange = false;
+		cuan = null;
+	}
+}
+
+void PickUp()
+{
+	if(cuan != null)
+	{
+		isHolding = true;
+		skullRb = cuan.GetComponent<Rigidbody>();
+		skullRb.linearVelocity = Vector3.zero;
+		skullRb.isKinematic = true;
+
+		skullRb.transform.parent = objectPosition;
+		skullRb.transform.localPosition = Vector3.zero;
+		skullRb.transform.localRotation = Quaternion.identity;
+	}
+	
+
+}
+
+void ThrowSkull()
+{
+
+}
+void SetUpControls()
+{
+	playerControls.Enable();
+	playerControls.Skull.Interact.performed += HandleSkull;
+}
+
+void OnDisable()
+{
+	playerControls.Disable();
+	playerControls.Skull.Interact.performed -= HandleSkull;
+}
+
+void HandleSkull(InputAction.CallbackContext context)
+{
+	if(inRange && !isHolding)
+	{
+		PickUp();
+	}
+	if(isHolding)
+	{
+		ThrowSkull();
+	}
 }
 }
