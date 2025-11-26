2976d9f1fb0eecf733dadabde69f0ae06d237179
Shin-Bins <sean_deans@hotmail.co.uk>
2025-11-26 22:12:00 +0000

    End of day save
    Okay the skull can ground slam and rotate to adjust the view when grounded. These mechanics are looking goo so far

--- Assets/Scripts/player_scripts/scripts_skull/SkullControl.cs
+++ Assets/Scripts/player_scripts/scripts_skull/SkullControl.cs
@@ -4,11 +4,15 @@ using UnityEngine.InputSystem;
 public class SkullControl : MonoBehaviour
 {
     public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
+    public float groundCheckRadius = 0.2f;//how big of an area we check for the ground. Keep small so we dont have a floating skull :)
     public LayerMask groundLayer;
     private bool isGrounded = true;
 
-    public float slamForce = 10;
+    public float slamForce = 10;//How hard the skull hits the ground
+
+    public float rotSpeed;//how fast it rotates left and right
+    private bool rotLeft = false;
+    private bool rotRight = false;
     private Rigidbody srb;
     private PlayerControls playerControls;
 
@@ -23,28 +27,66 @@ public class SkullControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer); 
+       isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+           if (rotLeft)
+        {
+            transform.Rotate(Vector3.up, -rotSpeed * Time.deltaTime, Space.Self);
+        }
+        if (rotRight)
+        {
+            transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime, Space.Self);
+        }
+
     }
 
     void GroundSlam(InputAction.CallbackContext context)
     {
-        while(isGrounded && transform.parent == null)
+        if(!isGrounded && transform.parent == null)
         {
-            srb.AddForce(-transform.up * slamForce);
+            srb.linearVelocity = -transform.up * slamForce;
 
         }
+    }
+//below are the bools for rotation. Needs to be in update for a smoth turn around
+   void RotLeftStart(InputAction.CallbackContext context)
+    {
+        rotLeft = true;
+    }
+
+    void RotLeftStop(InputAction.CallbackContext context)
+    {
+        rotLeft = false;
+    }
+
+    void RotRightStart(InputAction.CallbackContext context)
+    {
+        rotRight = true;
+    }
+
+    void RotRightStop(InputAction.CallbackContext context)
+    {
+        rotRight = false;
     }
 
     void SetupSkullControls()
     {
 	    playerControls.Enable();
 	    playerControls.Skull.Slam.performed += GroundSlam;
+        playerControls.Skull.RotateLeft.started += RotLeftStart;
+        playerControls.Skull.RotateLeft.canceled += RotLeftStop;
+        playerControls.Skull.RotateRight.started += RotRightStart;
+        playerControls.Skull.RotateRight.canceled += RotRightStop;
 
     }
 
     void OnDisable()
     {
         playerControls.Disable();
-        playerControls.Skull.Slam.performed -= GroundSlam;
+        playerControls.Skull.RotateLeft.started -= RotLeftStart;
+        playerControls.Skull.RotateLeft.canceled -= RotLeftStop;
+        playerControls.Skull.RotateRight.started -= RotRightStart;
+        playerControls.Skull.RotateRight.canceled -= RotRightStop;
+
     }
 }
