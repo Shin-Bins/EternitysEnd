df6949ad1762c3acae6aea29c82da91389fd5285
Shin-Bins <sean_deans@hotmail.co.uk>
2025-12-08 22:55:10 +0000

    Pop ups done
    Pop up dialogue works. Using the event triggers shane showed they start, disable player movement and use a button to continue to the next line. Each line is written in the inspector of the dialogue box object

--- Assets/Scripts/level_scripts/PopUpDialogue.cs
+++ Assets/Scripts/level_scripts/PopUpDialogue.cs
@@ -1,37 +1,71 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+
 public class PopUpDialogue : MonoBehaviour
 {
 
-public TMP_Text diaText;
-public float textSpeed;
-public string[] lines;
+    [System.Serializable]
+    public class DialogueLine
+    {
+        [TextArea(2, 4)]
+        public string text;
+        public Sprite charSprite; //container for all aspects of the dialogue. Can add more in here like audio to personalize the speaker. Will update later
+    }
 
+[SerializeField] TMP_Text diaText;
+[SerializeField] float textSpeed;
+[SerializeField] Image speakerImage;//this is the game object that displays the image set above
+[SerializeField] DialogueLine[] lines;
+[SerializeField] bool hasTriggered = false;//fixes the issue of retriggering dialogue. Might need another solution if we want dialogues to repeat.
 private int index;
 
+public PlayerInput cuan;
+public PlayerInput phiast;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
+
+        if (hasTriggered)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
          Cursor.visible = true;
          Cursor.lockState = CursorLockMode.None;
          diaText.text = string.Empty;
+         cuan.enabled = false;
+         phiast.enabled = false;
          StartDialogue();
     }
 
     void StartDialogue()
     {
         index = 0;
+        UpdateSpeaker();
         StartCoroutine(TypeLine());
       
     }
 
+    void UpdateSpeaker()
+    {
+        if (lines[index].charSprite != null)
+        {
+            speakerImage.sprite = lines[index].charSprite;
+            speakerImage.enabled = true;
+        }
+        else
+        {
+            speakerImage.enabled = false;
+        }
+    }
+
     IEnumerator TypeLine()
     {
-        foreach(char c in lines [index].ToCharArray())
+        foreach(char c in lines [index].text.ToCharArray())
         {
            diaText.text += c;
            yield return new WaitForSeconds(textSpeed);
@@ -44,10 +78,14 @@ private int index;
         {
             index ++;
             diaText.text = string.Empty;
+            UpdateSpeaker();
             StartCoroutine(TypeLine());
         }
         else
         {
+            hasTriggered = true;
+            cuan.enabled = true;
+            phiast.enabled = true;
             gameObject.SetActive(false);
         }
     }
