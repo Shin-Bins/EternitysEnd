69ae07fc0591baf1bcc0c88c87345a74d370dc7d
Shin-Bins <sean_deans@hotmail.co.uk>
2026-01-28 20:35:10 +0000

    Cutscene one blocked out
    UI and camera done for this. Need animations and to do the sound. Otherwise we gucci

--- Assets/Scripts/level_scripts/NextLevel.cs
+++ Assets/Scripts/level_scripts/NextLevel.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
 
-	[SerializeField]private string nextScene;//drag and drop the scene in here
+	[SerializeField]private string nextScene;//drop the scene name in here
 
 	public void NextArea()//shorter load, for levels in the same region
 	{
