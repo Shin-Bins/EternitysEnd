f1300aeefcd876a3f23706ed62964ce1b313da9a
GeorgeSeers <stephendonnelly1993@gmail.com>
2026-02-04 14:33:08 +0000

    respawn work not finished
    skull is fine, checkpoints are busted because for phis because unity hates non rigid body triggers will fix tommorrow

--- Assets/Scripts/level_scripts/KillBox.cs
+++ Assets/Scripts/level_scripts/KillBox.cs
@@ -1,20 +1,67 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class KillBox : MonoBehaviour
 {
-void OnTriggerEnter(Collider other)
+
+	public GameObject phis;
+	public GameObject skull;
+	public GameObject respawnpoint;
+	public Transform checka;
+	public GameObject check;
+	CharacterController cha;
+	
+
+    private void Start()
+    {
+		phis = GameObject.Find("Phiast-tank 1");
+		skull = GameObject.Find("skull");
+		respawnpoint = GameObject.Find("reset");
+		check = GameObject.Find("Checkpoint");
+		checka = check.transform;
+		cha = phis.GetComponent<CharacterController>();
+		phis.transform.position = checka.position;
+		
+        
+
+
+    }
+
+	public void pleasework()
+	{
+		cha.enabled = false;
+		phis.transform.position = checka.position;
+		cha.enabled = true;
+
+	}
+    void OnTriggerExit(Collider other)
 {
 	if(other.CompareTag("phiast"))
 	{
+
+		
 		PhiastHealth health = other.GetComponent<PhiastHealth>();
 		health.TakeDamage(transform.position);
-		GameManager.Instance.RespawnCheckpoint(other.gameObject);
+			pleasework();
+			
+
+		
+		
+			
+			
+			
+        //skull.transform.position = respawnpoint.transform.position;//
 		Debug.Log("Got da phiastie");
 	}
 	if(other.CompareTag("skull"))
 	{
-		GameManager.Instance.RespawnCheckpoint(other.gameObject);
+            phis.transform.position = respawnpoint.transform.position;
+            GameManager.Instance.RespawnCheckpoint(other.gameObject);
+		
+			
 	}
+
+
 
 }
 }
